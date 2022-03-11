5813323e49f06ad0a6faaf4e10929f31882b0c8e
RyanDeLong <rdelong111@gmail.com>
2022-03-11 15:41:15 -0600

    commit tree 5c241daafac78d96518a6f68f21ac9c8f456dedc

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -9,15 +9,14 @@ function ColorList() {
     "hotpink",
   ];
 
+  const colorElements = colors.map((Newcolor) => {
+    return <li key={Newcolor} style={{color: Newcolor}}>{Newcolor}</li>
+  })
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {colorElements}
       </ol>
     </div>
   );
